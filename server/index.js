23686b052afe7509988f5f5dcea1f58eaea1ec69
Chandan Kumar Saha <chandankumarshah8@gmail.com>
2023-05-11 13:05:24 +0530

    socket connection done

--- server/index.js
+++ server/index.js
@@ -1,11 +1,28 @@
 import express from "express";
-
+import http from 'http';
+import { Server } from "socket.io"
+import { fileURLToPath } from "url";
+import path from "path";
 const app = express();
 
+const httpServer = http.createServer(app);
+const io = new Server(httpServer);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 app.get('/', (req, res) => {
-    res.send("<h1>Hello There</h1>");
+    // res.send("<h1>Hello There</h1>");
+    res.sendFile(__dirname + "/index.html");
+
+
 })
 
-app.listen(5000, () => {
+io.on("connection", (socket) => {
+    console.log("Connection is ready");
+})
+
+httpServer.listen(5000, () => {
     console.log("Server running...");
 })
